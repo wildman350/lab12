3f4a14349842affcf578a2f0da78a2d6c0cdde17
wildman350 <s581776@student.roundrockisd.org>
2014-10-30 15:08:47 -0500

    70 pt done

--- 70pt.py
+++ 70pt.py
@@ -29,7 +29,11 @@ class MyApp:
 		self.button3.grid(row=0,column=2)	  ### (3)
 			
 	
-		
+	       
+		self.button4 = Button(self.myContainer1)
+		self.button4.configure(text="Goodbye", background="purple")  
+		self.button4.grid(row=0,column=3)	  ### (4)
+			 	
 root = Tk()
 myapp = MyApp(root)
 root.mainloop()
